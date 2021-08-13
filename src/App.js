e98ec706afe16020f7d3b0754858f7f7c37321e5
cortlandniccum <cortlandniccum@gmail.com>
2021-08-13 17:36:18 -0600

    push

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import './App.css';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [button, buttonPressed] = useState(false);
+  const buttonOn = (e) => {
+    buttonPressed(!button);
+  };
+  console.log(button);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  function User({ name }) {
+    const [character, setCharacter] = useState('');
+    const fetchUser = (name) =>
+      fetch('https://swapi.dev/api/people/${name}`)
+        .then(response => response.json())
+        .then(character => {
+          console.log(character);
+          setCharacter(character.name);
+        })
+  }
+
 
   return (
     <div className="App">
